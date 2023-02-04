51ed40148d7301b85fd5ec113ee973944c01bda4
morelKenzo <kenzo.morel29@gmail.com>
2023-02-04 11:59:02 +0100

    Petit commentaire

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import refpin as rp
 import tircharge
 
 while True:
+    # Test du tir du robot
     if button_a.is_pressed():
         tircharge.charge_condo(rp.pin_charge_condo)
     if button_b.was_pressed():
