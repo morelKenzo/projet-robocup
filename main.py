1426ee655efc906b39d61572c60654a2135ac404
morelKenzo <kenzo.morel29@gmail.com>
2023-02-10 11:46:52 +0100

    Ajout module mx12

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from microbit import *
 import refpin as rp
 import tircharge
+import mx12
 
 while True:
     # Test du tir du robot
     if button_a.is_pressed():
-        tircharge.charge_condo(rp.pin_charge_condo)
-    if button_b.was_pressed():
-        tircharge.commande_tir(rp.pin_charge_condo, pin_tir, 50)
+
--- refpin.py
+++ refpin.py
@@ -5,6 +5,14 @@ refpin.py :
     Les pins utilisées sont référencés dans
     ce module avec des noms clairs.
 """
-
+id_mot1 = 3
 pin_charge_condo = pin8
 pin_tir = pin16
+pin_dribbleur = pin0
+pin_tx = pin1
+pin_rx = pin2
+pin_led1 = pin13
+pin_led2 = pin14
+pin_led3 = pin15
+pin_scl = pin19
+pin_sda = pin20
