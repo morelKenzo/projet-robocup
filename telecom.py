b767b9f7e61bdf61564d6f9c3599cd8b6a6839c4
morelKenzo <kenzo.morel29@gmail.com>
2023-02-17 19:08:56 +0100

    Correction indentation

--- telecom.py
+++ telecom.py
@@ -8,7 +8,7 @@ radio.config(canal)
 radio.on()
 
 def reception():
-	message = radio.receive()
+    message = radio.receive()
     if message :
         listMessage = message.split(",") #on découpe le message d'entrée avec les virgules /!\ au protocole de communication avec Noah
         if listMessage[-1] == "fin":
@@ -18,4 +18,4 @@ def reception():
             tir = int(listMessage[3])
             passe = int(listMessage[4])
             dribble = int(listMessage[5])
-	return vit_x,vit_y,vit_rot,tir,passe,dribble #on retourne un tuple avec toutes les grandeurs
+    return vit_x,vit_y,vit_rot,tir,passe,dribble #on retourne un tuple avec toutes les grandeurs
