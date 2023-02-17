9f27fa74c5c67c6edba5fe1624424f57f42e0765
morelKenzo <kenzo.morel29@gmail.com>
2023-02-17 18:53:02 +0100

    Correction indentations

--- telecom.py
+++ telecom.py
@@ -3,33 +3,19 @@ import radio
 
 canal = 10 #definition du canal de communication avec le moteur.
 
-
-
 #initialisation de la radio
 radio.config(canal)
 radio.on()
 
-
-
-
-
 def reception():
 	message = radio.receive()
-        if message :
-            listMessage = message.split(",") #on découpe le message d'entrée avec les virgules /!\ au protocole de communication avec Noah
-            if listMessage[-1] == "fin":
-                
-                vit_x = int(listMessage[0]) #on découpe notre signal et on attribue les valeurs de vitesses
-                vit_y = int(listMessage[1])
-                vit_rot = int(listMessage[2])
-                tir = int(listMessage[3])
-                passe = int(listMessage[4])
-                dribble = int(listMessage[5])
-            
-	return vit_x,vit_y,vit_rot,tir,passe,dribble #on retourne un t-uple avec toutes les grandeurs
-
-
-
-
-
-	
+    if message :
+        listMessage = message.split(",") #on découpe le message d'entrée avec les virgules /!\ au protocole de communication avec Noah
+        if listMessage[-1] == "fin":
+            vit_x = int(listMessage[0]) #on découpe notre signal et on attribue les valeurs de vitesses
+            vit_y = int(listMessage[1])
+            vit_rot = int(listMessage[2])
+            tir = int(listMessage[3])
+            passe = int(listMessage[4])
+            dribble = int(listMessage[5])
+	return vit_x,vit_y,vit_rot,tir,passe,dribble #on retourne un tuple avec toutes les grandeurs
