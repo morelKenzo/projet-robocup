ff4a5ceda42737bf9a87ba2c74767f0c1619e833
morelKenzo <kenzo.morel29@gmail.com>
2023-02-17 08:28:57 +0100

    modification main

--- main.py
+++ main.py
@@ -3,8 +3,4 @@ import refpin as rp
 import tircharge
 import mx12
 
-roue1 = mx12.Roue(1)
-while True:
-    # Test du tir du robot
-    if button_a.is_pressed():
-        roue1.envoi_vitesse(500)
+# roue1 = mx12.Roue(1)
