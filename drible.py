0b41e9efb946991b82c566150dc079510aa70ac0
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2023-03-10 09:37:25 +0100

    Update drible.py

--- drible.py
+++ drible.py
@@ -1,8 +1,6 @@
 from refpin import pin_dribbleur #on trouve le pin attitré au dribbleur (pin0)
 
 
-
-
-def commandeDrible(prctVitesse):
+def commande_drible(prctVitesse):
         consigne = int(prctVitesse*1023/100) #on transforme notre pourcentage de vitesse en commande
-        pin_dribbleur.write_analog(consigne) #on envoie la consigne
+        pin_dribbleur.write_analog(consigne) #on envoie la consigne# 
